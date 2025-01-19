13b0f9fd9d2f3a80c8f28c5948431c0b93a467a3
healthycodingjava <healthycode@163.com>
2025-01-19 16:53:51 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker." + healthycodingjava]: dockerHub,
+  ["quay." + healthycodingjava]: "https://quay.io",
+  ["gcr." + healthycodingjava]: "https://gcr.io",
+  ["k8s-gcr." + healthycodingjava]: "https://k8s.gcr.io",
+  ["k8s." + healthycodingjava]: "https://registry.k8s.io",
+  ["ghcr." + healthycodingjava]: "https://ghcr.io",
+  ["cloudsmith." + healthycodingjava]: "https://docker.cloudsmith.io",
+  ["ecr." + healthycodingjava]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging." + healthycodingjava]: dockerHub,
 };
 
 function routeByHosts(host) {
